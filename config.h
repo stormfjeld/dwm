ec84c2f3483c115e16528b48b449d256f1e66a78
Storm Fjeldstad <stormisfjeld@gmail.com>
2021-08-14 22:01:28 +0200

    Added gaps and autostart

--- config.h
+++ config.h
@@ -1,7 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -86,6 +87,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_z,      setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_x,      setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_a,      setgaps,        {.i = 0  } },
 	{ MODKEY,                       XK_e,      shiftview,      {.i = +1 } },
 	{ MODKEY,                       XK_n,      shiftview,      {.i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
